5b49aeae0d1d906995577a645b2a780281f05d5f
Lucas Busso <lucasxbusso@gmail.com>
2022-04-05 11:32:06 -0300

    spiner de Carga para el CRUD

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,7 @@
+import '../styles/Spinner.css'
+
+export const Spinner = () => {
+  return (
+      <div className="spinner"></div>
+  )
+}
--- src/paginas/VerCliente.jsx
+++ src/paginas/VerCliente.jsx
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react"
+import { Spinner } from "../components/Spinner"
 
 const VerCliente = () => {
     const [ cliente, setCliente ] = useState({})
+    const [ cargando, setCargando] = useState(true)
 
     const { id } = useParams()
 
@@ -17,46 +19,58 @@ const VerCliente = () => {
         } catch (error) {
           
         }
+        setTimeout(() => {
+          setCargando(!cargando)
+        }, 3000);
       }
       obtenerClienteAPI()
     }, [])
-  return (
-    <div className="bg-white w-3/4 mx-auto p-10 rounded-md shadow-md">
-      <h1 className='font-black text-4xl text-blue-900'>Cliente:  {cliente.nombre}</h1>
-      <p className='mt-3 mb-10 text-xl border-b'>Información de cliente</p>
 
-      {cliente.nombre && (
-        <p className="text-4xl text-gray-700 p-2">
-          <span className="text-gray-800 uppercase font-bold ">Cliente: </span> 
-          {cliente.nombre}
-        </p>
-      )}
-      {cliente.email && (
-        <p className="text-2xl text-gray-700 p-2">
-          <span className="text-gray-800 uppercase font-bold ">Email: </span> 
-          {cliente.email}
-        </p>
-      )}
-      {cliente.telefono && (
-        <p className="text-2xl text-gray-700 p-2">
-          <span className="text-gray-800 uppercase font-bold ">Telefono: </span> 
-          {cliente.telefono}
-        </p>
-      )}
-      {cliente.empresa && (
-        <p className="text-2xl text-gray-700 p-2">
-          <span className="text-gray-800 uppercase font-bold ">Empresa: </span> 
-          {cliente.empresa}
-        </p>
-      )}
-      {cliente.notas && (
-        <p className="text-2xl text-gray-700 p-2">
-          <span className="text-gray-800 uppercase font-bold ">Notas: </span> 
-          {cliente.notas}
-        </p>
-      )}
+  return (
+    cargando ? <Spinner /> : 
+    Object.keys(cliente).length === 0 ? <p className="font-bold text-4xl text-gray-700 block text-center">No se encontraron los resultados</p> : (
+
+      <div className="bg-white w-3/4 mx-auto p-10 rounded-md shadow-md">
+          <>
+
+            <h1 className='font-black text-4xl text-blue-900'>Cliente:  {cliente.nombre}</h1>
+            <p className='mt-3 mb-10 text-xl border-b'>Información de cliente</p>
+
+            {cliente.nombre && (
+              <p className="text-4xl text-gray-700 p-2">
+                <span className="text-gray-800 uppercase font-bold ">Cliente: </span> 
+                {cliente.nombre}
+              </p>
+            )}
+            {cliente.email && (
+              <p className="text-2xl text-gray-700 p-2">
+                <span className="text-gray-800 uppercase font-bold ">Email: </span> 
+                {cliente.email}
+              </p>
+            )}
+            {cliente.telefono && (
+              <p className="text-2xl text-gray-700 p-2">
+                <span className="text-gray-800 uppercase font-bold ">Telefono: </span> 
+                {cliente.telefono}
+              </p>
+            )}
+            {cliente.empresa && (
+              <p className="text-2xl text-gray-700 p-2">
+                <span className="text-gray-800 uppercase font-bold ">Empresa: </span> 
+                {cliente.empresa}
+              </p>
+            )}
+            {cliente.notas && (
+              <p className="text-2xl text-gray-700 p-2">
+                <span className="text-gray-800 uppercase font-bold ">Notas: </span> 
+                {cliente.notas}
+              </p>
+            )}
+
+          </>   
+      </div>
       
-    </div>
+    )
   )
 }
 
